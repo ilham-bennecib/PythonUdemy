ed43296772a9789a8efde9475b7d119ae05369f4
KADRI <ilham.kadri@cgi.com>
2024-09-18 09:49:26 +0200

    refacto POO liste de course -  creation methodes elever et sauvegarder

--- tp/projet/liste course POO/lib.py
+++ tp/projet/liste course POO/lib.py
@@ -1,8 +1,16 @@
 #fichier qui contiendra les classes
 from dataclasses import dataclass
-
+import os
+import json
 import logging
+
+
+import constantes
+
+
 LOGGER = logging.getLogger()
+
+
 
 @dataclass
 class Liste(list):#on ajoute een parametre list pour que notre classe hérite des attribut
@@ -23,7 +31,12 @@ class Liste(list):#on ajoute een parametre list pour que notre classe hérite de
         self.append(element)
         return True #permet de verifier si l'element a bien été ajouté. 
  
- 
+    def afficher(self):
+        print(f"Ma liste : {self.nom}")
+        for elem in self:
+            print(f" -  {elem}")
+            
+            
     def enlever(self, element) :
         if element in self:
             self.remove(element)
@@ -32,14 +45,25 @@ class Liste(list):#on ajoute een parametre list pour que notre classe hérite de
         return False
  
  
- 
+    def sauvegarder(self):
+        #creation de la variable uicontient le chemin vers le fichier json
+        chemin= os.path.join(constantes.DATA_DIR, f"{self.nom}.json")    #on join le dossier data avec le nom de la liste
+        if not os.path.exists(constantes.DATA_DIR):                      #verification de l'existance du dossier
+            os.makedirs(constantes.DATA_DIR)
+        
+        with open(chemin, "w") as f:                                     # on ouvre le fichier en mode ecriture
+            json.dump(self,f,indent=4)                                   # ajout de notre liste+bonne indentation
+        
+        return True
+    
+    
  #execution du code quand on est dans cette page   
 if __name__ == "__main__":
         l=Liste("achat")
         l.ajouter("Pomme")
-        l.ajouter("Pomme")
-        l.enlever("Pomme")
-        print(l)
+        l.ajouter("Poire")
+        l.afficher()
+        l.sauvegarder()
    
         
         
